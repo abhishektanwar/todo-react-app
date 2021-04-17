8f07994e57e25e8a8124daf521aa46c5864724c0
=abhishektanwar <abhishektanwar084@gmail.com>
2021-04-17 11:10:04 +0530

    todo app completed locally

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -23,7 +23,7 @@ const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
 	}
 
 	return todos.map((todo,index)=>(
-		<div className={todo.isComplete ? "todo-row complete":"todo row"} key={index}>
+		<div className={todo.isComplete ? "todo-row complete":"todo-row"} key={index}>
 			<div key={todo.id} onClick = {() => {completeTodo(todo.id)}}>{todo.text}</div>
 		<div className="icons">
 			<RiCloseCircleLine onClick={() => {removeTodo(todo.id)}} className='delete-icon'/>
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,8 @@
 import { useState,useEffect,useRef } from 'react'
 
 const TodoForm = (props) => {
-	const [input,setInput] = useState("")
+	console.log('props',props)
+	const [input,setInput] = useState(props.edit ? props.edit.value : '')
 	const inputRef = useRef(null)
 	useEffect(() => {
 		inputRef.current.focus()
@@ -22,16 +23,37 @@ const TodoForm = (props) => {
 	}
 	return (
 		<form className="todo-form" onSubmit={handleSubmit}>
-			<input
-				type="text"
-				required
-				value={input}
-				placeholder="Add a new task"
-				className="todo-input"
-				onChange={handleChange}
-				ref={inputRef}
-			></input>
-			<button className="todo-button">Add Task</button>
+			{props.edit ? (
+				<>
+					<input
+						type="text"
+						required
+						value={input}
+						placeholder="Update task"
+						className="todo-input edit"
+						onChange={handleChange}
+						ref={inputRef}
+					></input>
+					<button className="todo-button edit">Update Task</button>
+				</>
+			) : (
+				<>
+					<input
+						type="text"
+						required
+						value={input}
+						placeholder="Add task"
+						className="todo-input"
+						onChange={handleChange}
+						ref={inputRef}
+					></input>
+					<button className="todo-button">Add Task</button>
+				</>
+			)
+				
+		}
+
+			
 		</form>
 	)
 }
